6656c4fb90d7bef5b3295079bc48795f05cc1bfb
Md Shahed Talukder <shahedtalukder51@gmail.com>
2021-07-13 22:37:08 +0600

    another commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-
 import './App.css';
+import axios from 'axios';
+import React,{useEffect, useState} from 'react';
 
 function App() {
+    const [post, setPost] = useState(null);
+    
+    useEffect(() => {
+        const num = random_num();
+        axios.get('https://bn-hadith-api.herokuapp.com/hadiths/'+num).then(res => {
+          setPost(res.data);
+        });
+    },[])
+
+
+    if(!post) return null
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <h1 className="hadith-name">{post.name}</h1>
+      <p className="hadith-description"><span>"</span>{post.description}<span>"</span></p>
+      <small className="hadith-ref">{post.references}</small>
+      <small className="grade">{post.grade}</small>
     </div>
   );
 }
+
+function random_num() {
+      return Math.floor(Math.random() * 65) + 1
+}
+
 
 export default App;
